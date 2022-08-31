4a0fd6c08776604a2074327f9dff311d5e5336c5
Shrikiran11 <92041814+Shrikiran11@users.noreply.github.com>
2022-08-31 15:26:45 +0530

    MergeTwoList31_08_22.java

---
+++ MergeTwoList.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+import java.util.stream.*;
+
+class MergeTwoList{
+ 
+   public static void main(String[] args){
+
+        	List arrList=new ArrayList();
+    
+        		arrList.add("fire");
+    
+        		arrList.add("Water");
+    
+        		arrList.add("sand");
+        
+       		 arrList.add("sky");
+         
+	List arrList2=new ArrayList();
+     
+	  	arrList.add("nerupu");
+   
+		 arrList.add("thani");
+  
+	     	arrList.add("manu");
+    
+	    	arrList.add("agayam");
+    
+	 List arrList3=new ArrayList();
+     
+	   	arrList.addAll(arrList2);
+       
+		 arrList3.addAll(arrList);
+       
+		 System.out.println(arrList3);
+  
+  	}
+
+}
