d46e994ca3e98bb7143d88dce1551038ba2c8666
Shrikiran11 <92041814+Shrikiran11@users.noreply.github.com>
2022-08-31 15:12:49 +0530

    SetToList_31_08_22.java

---
+++ SetToList.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+import java.util.stream.*;
+ 
+ 
+class SetToList {
+ 
+  
+  public static void main(String args[])
+    {
+ 
+ Set<String> set = new HashSet<String>();
+  
+     
+  // Add elements to set
+  
+     set.add("black");
+   
+     set.add("white");
+      
+  set.add("pink");
+     
+   set.add("venom");
+       
+ set.add("itis");
+       
+ System.out.println("\nSet: " + set+"\n");
+
+List<String> list = new ArrayList<>();
+
+//Method 2
+
+List<String> mylist = new ArrayList<>(set);
+     
+   for(String t:set){
+         
+   list.add(t);
+       
+ }
+     
+   System.out.println("\nList: " + list+"\n");
+   
+   System.out.println("\n2ndList: " + mylist);
+  
+  }
+    
+   
+ 
+}
